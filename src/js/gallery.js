d571dc8c0caadae0e3c67fe334c5a76c5f74e498
cefariron <adidaswww17@gmail.com>
2023-12-12 22:16:30 +0200

    Update gallery.js

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,4 +1,5 @@
 import { getPhotos } from "./pixabay-api";
+import Notiflix from "notiflix";
 
 const formEl = document.querySelector(".js-search-form")
 const listEl = document.querySelector(".js-gallery")
@@ -15,9 +16,18 @@ async function onSubmit(event) {
     querry = event.target.elements.searchQuery.value;
     
     try {
-        const response = await getPhotos(querry, page)
-        console.log(response);
+        const { data: { total, hits} } = await getPhotos(querry, page)
+        console.log(hits);
+        if(hits.length === 0){
+            return Notiflix.Notify.warning('Sorry, there are no images matching your search query. Please try again.');
+        }
     } catch (error) {
         console.log(error.message);
     }
+}
+
+function createMarkup(arr) {
+    return arr.map(item => `
+    
+    `)
 }
