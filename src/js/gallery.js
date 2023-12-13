8055a35956210129b0935043039089e0cc6e3b42
cefariron <adidaswww17@gmail.com>
2023-12-13 21:53:43 +0200

    Update gallery.js

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -28,6 +28,22 @@ async function onSubmit(event) {
 
 function createMarkup(arr) {
     return arr.map(item => `
-    
+    <div class="photo-card">
+  <img src="" alt="" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+    </p>
+  </div>
+</div>
     `)
 }
